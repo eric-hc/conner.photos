9a06bf6f61de9497ce962d7074e2d6e4a9689d58
Eric Carboni <ericcarboni@gmail.com>
2020-07-21 21:47:00 -0400

    feat: intial commit

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Conner Parker Photos',
+    description:
+      'Conner Parker is a photographer based in Raleigh, NC',
+    url: 'https://gatsby-starter-amsterdam.netlify.com',
+    author: 'Conner Parker',
+    image: 'https://gatsby-starter-amsterdam.netlify.com/og-image.jpg',
+    intro: 'Conner Parker is a photographer based in Raleigh, NC',
+    menuLinks: [
+      {
+        name: 'Home',
+        slug: '/',
+      },
+      {
+        name: 'Another Page',
+        slug: '/example/',
+      },
+    ],
+    footerLinks: [
+      {
+        name: 'Gatsby Theme Amsterdam',
+        url: 'https://github.com/ryanwiemer/gatsby-theme-amsterdam',
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-theme-amsterdam',
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Theme Amsterdam`,
+        short_name: `Amsterdam`,
+        background_color: `#f5f0eb`,
+        theme_color: `#f5f0eb`,
+        start_url: `/`,
+        display: `standalone`,
+        icon: require.resolve('./src/images/favicon.png'),
+      },
+    },
+  ],
+}
